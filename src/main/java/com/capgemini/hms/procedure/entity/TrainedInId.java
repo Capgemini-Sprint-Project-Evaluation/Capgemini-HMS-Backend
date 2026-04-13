4c112206ae3349293316f2091ca923857f0bf556
akashgaikwad28 <akash.gaikwad9945@gmail.com>
2026-04-13 09:39:44 +0530

    TrainedInId.java

--- src/main/java/com/capgemini/hms/procedure/entity/TrainedInId.java
+++ src/main/java/com/capgemini/hms/procedure/entity/TrainedInId.java
@@ -1,17 +1,49 @@
 package com.capgemini.hms.procedure.entity;
 
 import jakarta.persistence.Embeddable;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.io.Serializable;
+import java.util.Objects;
 
 @Embeddable
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class TrainedInId implements Serializable {
     private Integer physician;
     private Integer treatment;
+
+    public TrainedInId() {
+    }
+
+    public TrainedInId(Integer physician, Integer treatment) {
+        this.physician = physician;
+        this.treatment = treatment;
+    }
+
+    public Integer getPhysician() {
+        return physician;
+    }
+
+    public void setPhysician(Integer physician) {
+        this.physician = physician;
+    }
+
+    public Integer getTreatment() {
+        return treatment;
+    }
+
+    public void setTreatment(Integer treatment) {
+        this.treatment = treatment;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TrainedInId that = (TrainedInId) o;
+        return Objects.equals(physician, that.physician) && 
+               Objects.equals(treatment, that.treatment);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(physician, treatment);
+    }
 }
