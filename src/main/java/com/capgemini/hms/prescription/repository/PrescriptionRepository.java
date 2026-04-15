4b10ee2c8258b3dbef2cdca610accb1dd45eb36c
Aditya Jadhav <adityajadhav3117@gmail.com>
2026-04-15 16:10:35 +0530

    docs: add documentation to prescription repository

--- src/main/java/com/capgemini/hms/prescription/repository/PrescriptionRepository.java
+++ src/main/java/com/capgemini/hms/prescription/repository/PrescriptionRepository.java
@@ -7,8 +7,23 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+/**
+ * PrescriptionRepository provides standard JpaRepository operations for the Prescription entity.
+ * It includes custom finders to retrieve prescriptions by patient SSN or physician ID.
+ */
 @Repository
 public interface PrescriptionRepository extends JpaRepository<Prescription, PrescriptionId> {
+    /**
+     * Finds all prescriptions associated with a specific patient.
+     * @param ssn The social security number of the patient.
+     * @return A list of prescriptions.
+     */
     List<Prescription> findByPatient_Ssn(Integer ssn);
+
+    /**
+     * Finds all prescriptions issued by a specific physician.
+     * @param employeeId The employee ID of the physician.
+     * @return A list of prescriptions.
+     */
     List<Prescription> findByPhysician_EmployeeId(Integer employeeId);
 }
